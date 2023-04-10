e1e070337039c36fbb129798ae6c1b414014a0d8
ohusq <01nj253824@gmail.com>
2023-04-10 11:58:38 +0200

    Added README and License

--- main.py
+++ main.py
@@ -2,20 +2,34 @@ import string
 import random
 import requests
 from colorama import Fore, init
-import threading
+from os import system
+from time import sleep
 
 init()
 
-length = int(input("Enter the length of the usernames: "))
-
-authLink = "https://auth.roblox.com/v1/usernames/validate?birthday=2015-03-04T00:00:00.000Z&context=Signup&username="
 usedNames = []
+
+# Add the before generated usernames to the usedNames list
+with open("valid.txt", "r") as f:
+	for line in f:
+		usedNames.append(line.strip())
+
+with open("invalid.txt", "r") as f:
+	for line in f:
+		usedNames.append(line.strip())
+
+length = int(input("Enter the length of the usernames: "))
+authLink = "https://auth.roblox.com/v1/usernames/validate?birthday=2015-03-04T00:00:00.000Z&context=Signup&username="
+checkedUsers = 0 # Value for the amount of usernames checked in a minute
+
 
 def generate_username(length):
     chars = string.ascii_lowercase + string.digits
     return ''.join(random.choice(chars) for _ in range(length))
 
 def check_username(username):
+	global checkedUsers
+	checkedUsers += 1
 	# Check if the username is already used
 	if username in usedNames:
 		return
@@ -38,4 +52,12 @@ def loop():
 	while True:
 		check_username(generate_username(length))
 
+def resetCPM():
+	global checkedUsers
+	system("title CPM: " + str(checkedUsers) + " | Checked Users: " + str(len(usedNames)))
+	checkedUsers = 0
+	sleep(60)
+	resetCPM()
+
 loop()
+resetCPM()
