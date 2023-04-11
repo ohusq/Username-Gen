d6e3f2fd605442a8a93ff73fcd565c93962cfd18
ohusq <01nj253824@gmail.com>
2023-04-11 19:07:06 +0200

    Added proxy support and name counter.

--- main.py
+++ main.py
@@ -8,17 +8,37 @@ from time import sleep
 init()
 
 usedNames = []
+proxyList = []
+
+validNames = [] # List for the valid usernames
+invalidNames = [] # List for the invalid usernames
 
 # Add the before generated usernames to the usedNames list
 with open("valid.txt", "r") as f:
 	for line in f:
 		usedNames.append(line.strip())
+		validNames.append(line.strip())
 
 with open("invalid.txt", "r") as f:
 	for line in f:
 		usedNames.append(line.strip())
+		invalidNames.append(line.strip())
 
-length = int(input("Enter the length of the usernames: "))
+# Add the proxies to the proxyList list
+with open("proxies.txt", "r") as f:
+	for line in f:
+		proxyList.append(line.strip())
+
+print("Loaded " + str(len(proxyList)) + " proxies\n" + Fore.GREEN +"Valid usernames: " + str(len(validNames)) + "\n" + Fore.RED + "Invalid usernames: " + str(len(invalidNames)))
+
+length = int(input(Fore.RESET + "Enter the length of the usernames: "))
+useProxies = input("Use proxies? (y/n): ")
+if useProxies == "y":
+	pass
+else:
+	proxyList = [""]
+	print(Fore.YELLOW + "WARNING | Not using proxies is not recommended and unstable!")
+
 authLink = "https://auth.roblox.com/v1/usernames/validate?birthday=2015-03-04T00:00:00.000Z&context=Signup&username="
 checkedUsers = 0 # Value for the amount of usernames checked in a minute
 
@@ -29,16 +49,40 @@ def generate_username(length):
 
 def check_username(username):
 	global checkedUsers
-	checkedUsers += 1
+
 	# Check if the username is already used
 	if username in usedNames:
 		return
 	else:
-		usedNames.append(username)
+		pass
 
 	# Check if the username is valid
-	r = requests.get(authLink + username)
-	data = r.json()
+	if useProxies == "y": # If the user wants to use proxies
+		try:
+			currentProxy = random.choice(proxyList)
+		except:
+			print(Fore.RED + "ERROR | No proxies found in proxies.txt!")
+			sleep(5)
+			exit()
+	else:
+		currentProxy = ""
+	
+	try:
+		if currentProxy == "": # If the currentProxy is empty, don't use proxies
+			r = requests.get(authLink + username)
+			data = r.json()
+		else:
+			r = requests.get(authLink + username, proxies={"https": currentProxy})
+			data = r.json()
+	except:
+		print(Fore.RED + "ERROR | " + username + " | Reason: Invalid Proxy | Proxy: " + currentProxy)
+		proxyList.remove(currentProxy)
+		# Update the proxies.txt file
+		with open("proxies.txt", "w") as f:
+			for proxy in proxyList:
+				f.write(proxy + "\n")
+		return
+	
 	if data["code"] == 0:
 		print(Fore.GREEN + "VALID | " + username)
 		with open("valid.txt", "a") as f:
